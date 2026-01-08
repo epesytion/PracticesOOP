a29cbe849306fc6a858323f9f2454e486699f3fe
애페스 <uknaklz4@gmail.com>
2026-01-08 17:35:40 +0500

    firstcommit

---
+++ src/Student.java
@@ -0,0 +1,53 @@
+public class Student {
+    private int id;
+    private static int id_gen = 1;
+    private String name;
+    private String surname;
+    private double gpa;
+
+    Student(){ //empty construcotr
+        this.id = id_gen++;
+    }
+
+    Student(int id, String name, String surname){ //parametrized constructor
+        this(); //calling the empty constructor above (8-10 lines)
+        setName(name);
+        setSurname(surname);
+        setGpa(gpa);
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return id + ": " + name + " " + surname + " " + gpa;
+    }
+}
